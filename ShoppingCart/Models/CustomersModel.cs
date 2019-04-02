f21e4e2094c4ec4b204fe98cd5abebf1e16f477f
Charles Hongjun Wen <e0393579@u.nus.edu>
2019-04-02 10:04:57 +0800

    updated CustomerModel

--- ShoppingCart/Models/CustomersModel.cs
+++ ShoppingCart/Models/CustomersModel.cs
@@ -8,8 +8,8 @@ namespace ShoppingCart.Models
 {
     public class CustomersModel
     {
-        [Required(ErrorMessage = "Customer ID is required")]
-        [Display(Name = "Customer ID")]
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
         public int CustomerId { get; set; }
         [Required(ErrorMessage = "Password is required")]
         [Display(Name = "Password")]
