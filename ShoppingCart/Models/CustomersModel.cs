789d34217b816095215dfb7f8cd141ca651c2d9f
Aung Kaung Myat <akm97akm@gmail.com>
2019-04-02 10:01:10 +0800

    created CustomerModel for login by Aung

---
+++ ShoppingCart/Models/CustomersModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingCart.Models
+{
+    public class CustomersModel
+    {
+        [Required(ErrorMessage = "Customer ID is required")]
+        [Display(Name = "Customer ID")]
+        public int CustomerId { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SessionId { get; set; }
+    }
+}
