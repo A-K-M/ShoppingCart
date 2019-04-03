cc30eba06b957f30c4cb018be94d2c15c8240834
Annapoorani Mahadevan <e0384974@u.nus.edu>
2019-04-03 16:45:30 +0800

    update changes in index

--- ShoppingCart/Controllers/PurchasesController.cs
+++ ShoppingCart/Controllers/PurchasesController.cs
@@ -11,9 +11,9 @@ namespace ShoppingCart.Controllers
     public class PurchasesController : Controller
     {
         // GET: Purchases
-        public ActionResult Index(string PurchaseId)
+        public ActionResult Index(string SessionId)
         {
-            List<Purchase> Purchase = PurchaseData.GetPurchaseByPurchaseId(PurchaseId);
+            List<Purchase> Purchase = PurchaseData.GetPurchaseBySessionId(SessionId);
             ViewData["Purchase"] = Purchase;
            
             return View();
