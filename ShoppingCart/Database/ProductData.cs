13c2449ac5518a9a70d24bdd17c9229ba9d88fb1
Aung Kaung Myat <akm97akm@gmail.com>
2019-04-04 13:34:54 +0800

    add search

--- ShoppingCart/Database/ProductData.cs
+++ ShoppingCart/Database/ProductData.cs
@@ -17,7 +17,7 @@ namespace ShoppingCart.Database
             {
                 conn.Open();
 
-                string sql = @"SELECT ProductId, ProductName, ProductDescription, UnitPrice, Image
+                string sql = @"SELECT ProductId, ProductName, ProductDescription, UnitPrice, ImagePath
                             from Products order by ProductId";
                 SqlCommand cmd = new SqlCommand(sql, conn);
 
@@ -34,12 +34,40 @@ namespace ShoppingCart.Database
                             ProductName = (string)reader["ProductName"],
                             ProductDescription = (string)reader["ProductDescription"],
                             UnitPrice = (decimal)reader["UnitPrice"],
-                            ImagePath = (string)reader["Image"]
+                            ImagePath = (string)reader["ImagePath"]
                         });
                     }
                 }
             }
             return products_list;
+        }
+
+        public List<Product> GetSearchProducts(string searchObj)
+        {
+            List<Product> Searched_products_list = new List<Product>();
+            using (SqlConnection conn = new SqlConnection(Data.connectionString))
+            {
+                conn.Open();
+                string sql = @"SELECT * FROM Products where ProductName like '%" + searchObj + "%' " +
+                    "or ProductDescription like '%" + searchObj + "%'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader != null)
+                {
+                    while (reader.Read())
+                    {
+                        Searched_products_list.Add(new Product()
+                        {
+                            ProductId = (int)reader["ProductId"],
+                            ProductName = (string)reader["ProductName"],
+                            ProductDescription = (string)reader["ProductDescription"],
+                            UnitPrice = (decimal)reader["UnitPrice"],
+                            ImagePath = (string)reader["ImagePath"]
+                        });
+                    }
+                }
+                return Searched_products_list;
+            }
         }
     }
 }
